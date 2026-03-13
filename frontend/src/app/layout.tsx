c915ee73453b8d2ccf051b6d16e344709e3e5f2f
Aditya <Aditya.Yadav@INNOFarms.AI>
2026-03-13 17:49:41 +0530

    added scripts in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -56,10 +56,20 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vv38q8ux40");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${spartan.variable} ${outfit.variable} antialiased flex flex-col min-h-screen`}
       >
+        <GoogleTagManager gtmId="GTM-WVLHXKFZ" />
         {/* 1. Navbar Sticky Wrapper */}
         {/* sticky: keeps it in flow but sticks on scroll */}
         {/* top-0: sticks to the very top */}
@@ -76,7 +86,6 @@ export default function RootLayout({
         {/* 3. Footer */}
         <Footer />
       </body>
-      <GoogleTagManager gtmId="GTM-WVLHXKFZ" />
     </html>
   );
 }
